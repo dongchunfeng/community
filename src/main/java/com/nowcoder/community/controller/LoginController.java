fc6c941ebdf48cbf556e8b4973c38e0ef1aab718
dongchunfeng <dcf_free@126.com>
2020-09-15 20:07:02 +0800

    生成验证码

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.nowcoder.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityConstant;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,14 +12,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@Slf4j
 public class LoginController implements CommunityConstant {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private Producer kaptchaProducer;
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -61,6 +73,22 @@ public class LoginController implements CommunityConstant {
             model.addAttribute("target","/index");
         }
         return "/site/operate-result";
+    }
+
+    @RequestMapping(value="/kaptcha",method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response,HttpSession session){
+        String text = kaptchaProducer.createText();//验证码
+        BufferedImage image = kaptchaProducer.createImage(text);
+        session.setAttribute("kaptcha",text);
+        //将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream outputStream = response.getOutputStream();
+            ImageIO.write(image,"png",outputStream);
+        } catch (IOException e) {
+            log.error("响应验证码失败:"+e.getMessage());
+        }
+
     }
 
 
